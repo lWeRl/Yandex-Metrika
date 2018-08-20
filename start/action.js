c407c19d90d215d00c7e745e2c41d7d93f6e2b03
Nickolay Bobrov <werasdas@gmail.com>
2018-08-20 15:06:37 +0300

    Тесты с яндекс метрикрой: лог в консоль.

--- start/action.js
+++ start/action.js
@@ -5,18 +5,22 @@
     $("#open").on("click", function () {
         dialog.showModal();
         yaCounter.reachGoal("open");
+        console.log('opened, sent open');
     });
 
     $("#button2").on("click", function () {
-        yaCounter.reachGoal("button2", function(){dialog.close()});
+        yaCounter.reachGoal("right_button");
+        console.log('sent right');
     });
 
     $("#button1").on("click", function () {
-        yaCounter.reachGoal("button1", function(){dialog.close()});
+        yaCounter.reachGoal("left_button");
+        console.log('sent left');
     });
 
     dialog.onclose = function () {
-        yaCounter.reachGoal("send");
+        yaCounter.reachGoal("close");
+        console.log('closed, sent close');
     };
 
 }());
